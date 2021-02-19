c6564b907380b581206cf905cdf8db8f10489a6b
liusijie02 <liusijie02@meituan.com>
2021-02-20 00:30:42 +0800

    todo

--- pages/checkIn/index.js
+++ pages/checkIn/index.js
@@ -17,7 +17,7 @@ Page({
     tel: '',
     idCard: '',
     travelCodeImgs: [],
-    healthCodeImgs: [{url: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=236151376,2917765395&fm=26&gp=0.jpg'}],
+    healthCodeImgs: [],
     focusIdx: 7
   },
 
@@ -145,22 +145,4 @@ Page({
     })
   },
 
-  // 文件上传
-  afterRead2(event) {
-    console.log(event)
-    const { file } = event.detail;
-    // 当设置 mutiple 为 true 时, file 为数组格式，否则为对象格式
-    wx.uploadFile({
-      url: 'https://example.weixin.qq.com/upload', // 仅为示例，非真实的接口地址
-      filePath: file.url,
-      name: 'file',
-      formData: { user: 'test' },
-      success(res) {
-        // 上传完成需要更新 fileList
-        const { fileList = [] } = this.data;
-        fileList.push({ ...file, url: res.data });
-        this.setData({ fileList });
-      },
-    });
-  },
 })
